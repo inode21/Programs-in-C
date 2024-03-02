8526d886b4336528ca228338780aec61c46eedb1
Vaishnavi Karemore <vaishnavi.y.karemore@gmail.com>
2024-03-02 14:21:37 +0530

    Update DecreasingFactor.c

--- DecreasingFactor.c
+++ DecreasingFactor.c
@@ -4,5 +4,30 @@
 
 void DisplayFactor(int iNo)
 {
-    
+    int i = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i = iNo/2; i > 0; i--)
+    {
+        if((iNo % i)== 0)
+        {
+            printf("%d\t",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    DisplayFactor(iValue);
+
+    return 0;
 }
